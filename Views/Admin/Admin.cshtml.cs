4b9a7ede39f1942442d953bf6c47934c87d294fc
Michal Mikl <ionsibre@gmail.com>
2022-10-04 12:34:47 +0200

    Init

---
+++ Views/Admin/Admin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DutyFree.Pages
+{
+    public class AdminModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Views/UserOrders/UserOrders.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DutyFree.Pages
+{
+    public class UserOrdersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
