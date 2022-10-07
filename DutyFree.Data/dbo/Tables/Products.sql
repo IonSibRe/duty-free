7ea595bce10cf307eb716f7887c9b2f1d76abe3a
Michal Mikl <ionsibre@gmail.com>
2022-10-07 13:21:24 +0200

    reorganize

---
+++ DutyFree.Data/dbo/Tables/Products.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Products] (
+    [ProductId]   INT            IDENTITY (1, 1) NOT NULL,
+    [DateCreated] DATETIME2 (7)  NOT NULL,
+    [CreatedBy]   INT            NOT NULL,
+    [DateUpdated] DATETIME2 (7)  NULL,
+    [UpdatedBy]   INT            NULL,
+    [isDeleted]   BIT            NOT NULL,
+    [Name]        NVARCHAR (MAX) NOT NULL,
+    [Price]       INT            NOT NULL,
+    [Quantity]    INT            NOT NULL,
+    [ImageUrl]    NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED ([ProductId] ASC)
+);
+
