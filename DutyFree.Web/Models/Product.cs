f2afa324ac228793b644fa4b40d95c61f18fafba
Jafaros <57791098+Jafaros@users.noreply.github.com>
2022-10-10 12:00:05 +0200

    Admin completed

--- DutyFree.Web/Models/Product.cs
+++ DutyFree.Web/Models/Product.cs
@@ -1,21 +1,23 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DutyFree.Web.Models
 {
     public class Product
     {
-        [Key]
         public int ProductId { get; set; }
+        [Display(Name = "Product")]
+        public string? Name { get; set; }
+        [Display(Name = "Price")]
+        public int Price { get; set; }
         public DateTime DateCreated { get; set; } = DateTime.Now;
         public int CreatedBy { get; set; }
         public DateTime DateUpdated { get; set; }
-        public int UpdatedBy { get; set; }
         public bool isDeleted { get; set; }
-        public string Name { get; set; }
-        public int Price { get; set; }
-        public int  Quantity { get; set; }
-        public string ImageUrl { get; set; }
-        //public IFormFile Image { get; set; }
-
+        [Display(Name = "Available amount")]
+        public int Quantity { get; set; }
+        public string? ImageUrl { get; set; }
+        //[Required(ErrorMessage = "Please select file")]
+        //public IFormFile? Image { get; set; }
     }
 }
