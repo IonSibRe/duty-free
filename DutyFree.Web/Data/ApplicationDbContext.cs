c4b8ec9196496324a4b8c7d66fd59c831121ebaf
Jafaros <57791098+Jafaros@users.noreply.github.com>
2022-10-11 14:50:52 +0200

    Merge fix

--- DutyFree.Web/Data/ApplicationDbContext.cs
+++ DutyFree.Web/Data/ApplicationDbContext.cs
@@ -12,10 +12,7 @@ namespace DutyFree.Web.Data
 
         public DbSet<Product> Products { get; set; }
         public DbSet<User> Users { get; set; }
-<<<<<<< Updated upstream
         public DbSet<Order> Orders { get; set; }
-=======
         public DbSet<Category> Categories { get; set; } 
->>>>>>> Stashed changes
     }
 }
